ffd03c39b1577a836f019d4c10812a3990d51304
nguyendev2018 <nguyendev2018@gmail.con>
2021-11-20 14:44:47 +0700

    --- Auto-Git Commit ---

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -16,10 +16,14 @@ const filter_buttons = document.querySelectorAll('.product-details .product-deta
 
 const filter_box = document.querySelectorAll('.product-details--item .box-item');
 Array.from(filter_buttons).forEach(filter_button =>
+    // 
     filter_button.addEventListener("click", function () {
         for (let i = 0; i < filter_buttons.length; i++) {
             filter_buttons[i].classList.remove("active")
         }
         this.classList.add("active");
+        let data_button = filter_button.dataset.filter;
+        console.log(data_button);
     })
+
 )
