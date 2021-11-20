5fbccc1a81a3472a99f52d7a7aeff1445453a8b1
nguyendev2018 <nguyendev2018@gmail.con>
2021-11-20 15:15:03 +0700

    --- Auto-Git Commit ---

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -14,7 +14,7 @@ bg.onclick = function () {
 // filter
 const filter_buttons = document.querySelectorAll('.product-details .product-details--list');
 
-const filter_box = document.querySelectorAll('.product-details--item .box-item');
+const filter_boxs = document.querySelectorAll('.product-details--item .box-item');
 Array.from(filter_buttons).forEach(filter_button =>
     // 
     filter_button.addEventListener("click", function () {
@@ -23,7 +23,16 @@ Array.from(filter_buttons).forEach(filter_button =>
         }
         this.classList.add("active");
         let data_button = filter_button.dataset.filter;
-        console.log(data_button);
+        Array.from(filter_boxs).forEach(filter_box => {
+            let data_box = filter_box.dataset.item;
+            if(data_box === data_button || data_button ==="all"){
+                filter_box.style.display = "grid"
+             } 
+             else{
+                filter_box.style.display = "none"
+             }
+        });
     })
-
+    
+   
 )
