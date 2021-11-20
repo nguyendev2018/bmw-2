f218bafaa9dc1e688732a521f3498441b30beda4
nguyendev2018 <nguyendev2018@gmail.con>
2021-11-20 09:58:33 +0700

    --- Auto-Git Commit ---

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -3,19 +3,23 @@ const button = document.querySelector(".video-play--btn");
 const video = document.querySelector(".video-clip");
 const bg = document.querySelector(".bg-clip");
 const close = document.querySelector(".close");
-button.onclick = function(){
+button.onclick = function () {
     video.classList.add("show");
     bg.classList.add('show');
 }
-bg.onclick = function(){
+bg.onclick = function () {
     video.classList.remove("show");
     bg.classList.remove('show');
-} 
+}
 // filter
-const filter_buttons = document.querySelectorAll('.product-details--item .box-item');
-const filter_box = document.querySelectorAll('.product-details .product-details--list');
-// Array.from(filter_buttons).forEach(filter_button => 
-//     filter_button.addEventListener("click",function(){
-//         console.log("nguyen");
-//     })
-//     )
+const filter_buttons = document.querySelectorAll('.product-details .product-details--list');
+
+const filter_box = document.querySelectorAll('.product-details--item .box-item');
+Array.from(filter_buttons).forEach(filter_button =>
+    filter_button.addEventListener("click", function () {
+        for (let i = 0; i < filter_button.length; i++) {
+            filter_button[i].classList.remove("active")
+        }
+        this.classList.add("active");
+    })
+)
