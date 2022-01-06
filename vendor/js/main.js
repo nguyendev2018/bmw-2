6b3775ead811788a94299865061e49fdc84775af
nguyendev2018 <nguyendev2018@gmail.con>
2022-01-06 22:01:08 +0700

    --- Auto-Git Commit ---

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -3,21 +3,21 @@ const button = document.querySelector(".video-play--btn");
 const video = document.querySelector(".video-clip");
 const bg = document.querySelector(".bg-clip");
 const close = document.querySelector(".close");
-button.onclick = function () {
+button.onclick = function() {
     video.classList.add("show");
     bg.classList.add('show');
 }
-bg.onclick = function () {
-    video.classList.remove("show");
-    bg.classList.remove('show');
-}
-// filter
+bg.onclick = function() {
+        video.classList.remove("show");
+        bg.classList.remove('show');
+    }
+    // filter
 const filter_buttons = document.querySelectorAll('.product-details .product-details--list');
 
 const filter_boxs = document.querySelectorAll('.product-details--item .box-item');
 Array.from(filter_buttons).forEach(filter_button =>
     // 
-    filter_button.addEventListener("click", function () {
+    filter_button.addEventListener("click", function() {
         for (let i = 0; i < filter_buttons.length; i++) {
             filter_buttons[i].classList.remove("active")
         }
@@ -25,14 +25,13 @@ Array.from(filter_buttons).forEach(filter_button =>
         let data_button = filter_button.dataset.filter;
         Array.from(filter_boxs).forEach(filter_box => {
             let data_box = filter_box.dataset.item;
-            if(data_box === data_button || data_button ==="all"){
+            if (data_box === data_button || data_button === "all") {
                 filter_box.style.display = "grid"
-             } 
-             else{
+            } else {
                 filter_box.style.display = "none"
-             }
+            }
         });
     })
-    
-   
+
+
 )
