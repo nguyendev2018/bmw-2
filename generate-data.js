6f06c05a3a5670065a3a10d9282d52450d712b46
nguyendev2018 <nguyendev2018@gmail.con>
2022-01-13 22:26:28 +0700

    --- Auto-Git Commit ---

--- generate-data.js
+++ generate-data.js
@@ -1,5 +1,6 @@
-let faker = require('faker');
-// fake ra ngôn ngữ tiếng việt
-faker.locale = 'vi';
-// random data
-console.log(faker.commerce.department());
+// var faker = require('faker');
+// var randomName = faker.name.findName(); // Rowan Nikolaus
+// var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+// var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+// console.log(randomName);
+console.log('sdsd');
