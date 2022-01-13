03537a0f2a47e0248996acae1457c63438105a49
nguyendev2018 <nguyendev2018@gmail.con>
2022-01-13 13:19:21 +0700

    --- Auto-Git Commit ---

--- generate-data.js
+++ generate-data.js
@@ -1,10 +1,9 @@
-// const fake = require("faker");
+const fake = require("faker");
 // fake ra ngôn ngữ tiếng việt
-// fake.locale = 'vi';
+fake.locale = 'vi';
 // random data
-console.log('sds');
-// console.log(fake.commerce.department());
-// console.log(faker.commerce.productName());
-// console.log(faker.commerce.productDescription());
-// console.log(faker.image.imageUrl());
-// console.log(faker.name.findName());
+console.log(fake.commerce.department());
+console.log(faker.commerce.productName());
+console.log(faker.commerce.productDescription());
+console.log(faker.image.imageUrl());
+console.log(faker.name.findName());
