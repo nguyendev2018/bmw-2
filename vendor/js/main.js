566ad96bea2ffedf35f1244e2390a494d565a927
nguyendev2018 <nguyendev2018@gmail.con>
2022-01-16 15:39:17 +0700

    --- Auto-Git Commit ---

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -16,22 +16,28 @@ const filter_buttons = document.querySelectorAll('.product-details .product-deta
 
 const filter_boxs = document.querySelectorAll('.product-details--item .box-item');
 Array.from(filter_buttons).forEach(filter_button =>
-    // 
-    filter_button.addEventListener("click", function() {
-        for (let i = 0; i < filter_buttons.length; i++) {
-            filter_buttons[i].classList.remove("active")
-        }
-        this.classList.add("active");
-        let data_button = filter_button.dataset.filter;
-        Array.from(filter_boxs).forEach(filter_box => {
-            let data_box = filter_box.dataset.item;
-            if (data_box === data_button || data_button === "all") {
-                filter_box.style.display = "grid"
-            } else {
-                filter_box.style.display = "none"
+        // 
+        filter_button.addEventListener("click", function() {
+            for (let i = 0; i < filter_buttons.length; i++) {
+                filter_buttons[i].classList.remove("active")
             }
-        });
-    })
+            this.classList.add("active");
+            let data_button = filter_button.dataset.filter;
+            Array.from(filter_boxs).forEach(filter_box => {
+                let data_box = filter_box.dataset.item;
+                if (data_box === data_button || data_button === "all") {
+                    filter_box.style.display = "grid"
+                } else {
+                    filter_box.style.display = "none"
+                }
+            });
+        })
 
 
-)
+    )
+    // scrollUp 
+function scrollUp() {
+    const scrollUp = document.querySelector('.scroll-up');
+    this.scrollY > 350 ? scrollUp.classList.add("active") : scrollUp.classList.remove("active")
+}
+window.addEventListener("scroll", scrollUp)
